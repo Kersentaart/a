f17bba7943b48651cdeebb308dc74f33f1c7c743
Uros Hren <uros.hren@dlabs.si>
2021-03-16 17:25:12 +0100

    update file linking

---
+++ uploadFiles.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+link() {
+  echo "This utility will symlink the files in $HOME/.config directory to this repo"
+  echo "Do you want work or home directories?"
+
+  read directory
+
+  if [ "$directory" = 'home' -o "$directory" = 'work' ] ; then
+
+    echo "Proceed with ${directory}? (y/n)"
+    read resp
+
+    if [ "$resp" = 'y' -o "$resp" = 'Y' ] ; then
+
+      for file in $( ls -A $PWD/$directory | grep -vE '.*exclude.*|\.git|\.gitignore|.*.md') ; do
+        mv "$PWD/$directory/$file" "$PWD/.old_dotfiles_repo/$directory"
+      done
+
+      for file in $( ls -A $HOME/.config | grep -vE '.*exclude.*|\.git|\.gitignore|.*.md' ) ; do
+        ln -fsv "$HOME/.config" "$PWD/$directory/$file"
+      done
+
+      echo "Symlinking complete"
+    else
+
+      echo "Symlinking cancelled by user"
+      return 1
+
+    fi
+  else
+
+    echo "Wrong input"
+    return 1
+
+  fi
+}
+
+link
